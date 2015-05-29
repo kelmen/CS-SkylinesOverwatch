92f40acad6f013f03845e14aed711dced7f20ea7
kelmen <kelmen@hotmail.com>
2015-05-30 03:10:48 +0800

    revise

--- Loader.cs
+++ Loader.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using ICities;
 using ColossalFramework;
 using ColossalFramework.Math;
+using ColossalFramework.Plugins;
 using ColossalFramework.UI;
 using UnityEngine;
 
@@ -14,17 +15,23 @@ namespace SkylinesOverwatch
     {
         Helper _helper;
 
-        public void OnCreated(ILoading loading)
+        public Loader()
         {
             _helper = Helper.Instance;
+        }
 
+        public void OnCreated(ILoading loading)
+        {
             _helper.GameLoaded = loading.loadingComplete;
         }
 
         public void OnLevelLoaded(LoadMode mode)
         {
             if (mode == LoadMode.NewGame || mode == LoadMode.LoadGame)
+            {
+                //DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, string.Format("SkylinesOverwatch.OnLevelLoaded 1.1 {0}", _helper == null));
                 _helper.GameLoaded = true;
+            }
         }
 
         public void OnLevelUnloading()
